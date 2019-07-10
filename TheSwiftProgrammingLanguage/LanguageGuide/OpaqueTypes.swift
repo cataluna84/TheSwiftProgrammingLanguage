161c12cc0d918e10871e009f913f464bfef26f8a
Mayank Bhaskar <mayank.bhaskar@infobeans.com>
2019-07-10 19:55:35 +0530

    Hiding type information via Opaque Types introduced in Swift 5.1

--- TheSwiftProgrammingLanguage/LanguageGuide/OpaqueTypes.swift
+++ TheSwiftProgrammingLanguage/LanguageGuide/OpaqueTypes.swift
@@ -42,7 +42,22 @@ func opaqueTypesMain() {
 //     **
 //     *
 
+    let opaqueJoinedTriangles = join(smallTriangle, flip(smallTriangle))
+    print(opaqueJoinedTriangles.draw())
+//    *
+//    **
+//    ***
+//    ***
+//    **
+//    *
     
+
+// Swift can infer associated types, which lets you use an opaque return value in places where a protocol type can’t be used as a return value.
+// Using associatedType in ContainerOpaque protocol
+    let opaqueContainer = makeOpaqueContainer(item: 12)
+    let twelve = opaqueContainer[0]
+    print(type(of: twelve))
+    // prints: "Int"
 }
 
 // MARK: The problem that opaque types solve
@@ -108,4 +123,70 @@ func makeTrapezoid() -> some Shape {
     let trapezoid = JoinedShape(top: top, bottom: JoinedShape(top: middle, bottom: bottom))
     
     return trapezoid
+}
+
+// MARK: Combining opaque return types with generics
+func flip<T: Shape>(_ shape: T) -> some Shape {
+    FlippedShape(shape: shape)      // implicit return added in Swift 5.1
+}
+func join<T: Shape, U: Shape>(_ top: T, _ bottom: U) -> some Shape {
+    JoinedShape(top: top, bottom: bottom)
+}
+
+/*
+ If a function with an opaque return type returns from multiple places, all of the possible return values must have the same type. For a generic function, that return type can use the function’s generic type parameters, but it must still be a single type.
+ */
+// MARK: Error with the following function
+/*
+func invalidFlip<T: Shape>(_ shape: T) -> some Shape {
+    if shape is Square {
+        return shape
+    }
+    
+    return FlippedShape(shape: shape)
+}
+ */
+
+/*
+ The requirement to always return a single type doesn’t prevent you from using generics in an opaque return type. Here’s an example of a function that incorporates its type parameter into the underlying type of the value it returns:
+ */
+func `repeat`<T: Shape>(shape: T, count: Int) -> some Collection {
+    return Array<T>(repeating: shape, count: count)
+}
+
+// MARK: Differences Between Opaque Types and Protocol Types
+/*
+ protoFlip(_:) makes a much looser API contract with its caller than invalidFlip(_:) makes. It reserves the flexibility to return values of multiple types:
+ */
+func protoFlip<T: Shape>(_ shape: T) -> Shape {
+    if shape is Square {
+        return shape
+    }
+    
+    return FlippedShape(shape: shape)
+}
+
+// Opaque types preserve the identity of the underlying type
+protocol ContainerOpaque {
+    associatedtype Item
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+}
+
+extension Array: ContainerOpaque { }
+
+// MARK: Error: Protocol with associated types can't be used as a return type.
+/*
+func makeProtocolContainer<T>(item: T) -> ContainerOpaque {
+    return [item]
+}
+// MARK: Error: Not enough information to infer C.
+func makeProtocolContainer<T, C: ContainerOpaque>(item: T) -> C {
+    return [item]
+}
+ */
+
+
+func makeOpaqueContainer<T>(item: T) -> some ContainerOpaque {
+    return [item]
 }
--- TheSwiftProgrammingLanguage/ViewController.swift
+++ TheSwiftProgrammingLanguage/ViewController.swift
@@ -27,6 +27,8 @@ class ViewController: UIViewController {
         
         propertiesMain()
         
+        opaqueTypesMain()
+        
         ControlFlow.controlTransferStatement()
         
         
